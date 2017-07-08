e58df13536b50566e9db149df738345d4ff7ecee
jeremie.bailleul <jeremie.bailleul@gmail.com>
2017-07-08 19:15:35 +0200

    Linking with backend

--- src/app/services/riot-api.service.ts
+++ src/app/services/riot-api.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class RiotApiService {
 
-  constructor() { }
+  private _backendURL: any;
+
+  constructor(private _http: Http) {
+    this._backendURL = {};
+
+    // build backend base url
+    let baseUrl = `${environment.backend.protocol}://${environment.backend.host}`;
+    if (environment.backend.port) {
+      baseUrl += `:${environment.backend.port}`;
+    }
+
+    // build all backend urls
+    Object.keys(environment.backend.endpoints).forEach(k => this._backendURL[k] = `${baseUrl}${environment.backend.endpoints[k]}`);
+  }
+
+  getSummonerByName(): Observable<Object> {
+    return this._http.get(this._backendURL.getSummonerByName, this._options())
+      .map((res: Response) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return {};
+        }
+      });
+  }
+
+  private _options(headerList: Object = {}): RequestOptions {
+    const headers = new Headers(Object.assign({ 'Content-Type': 'application/json' }, headerList));
+    return new RequestOptions({ headers: headers });
+  }
 
 }
